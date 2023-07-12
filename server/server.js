6eb0db0b8ffe0c600666af891922638072eebc6b
BenediktDornauer <benedikt.dornauer@outlook.at>
2023-07-12 10:47:17 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,22 @@
-const http = require('http')
-const fs = require('fs')
+const http = require("http");
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'content-type': 'text/html' })
-    fs.createReadStream('index.html').pipe(res)
-})
+  const urlPath = req.url;
+  if (urlPath === "/overview") {
+    res.end('Welcome to the "overview page" of the nginX project');
+  } else if (urlPath === "/api") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(
+      JSON.stringify({
+        product_id: "xyz12u3",
+        product_name: "NginX injector",
+      })
+    );
+  } else {
+    res.end("Successfully started a server");
+  }
+});
 
-server.listen(process.env.PORT || 3000)
+server.listen(3000, "localhost", () => {
+  console.log("Listening for request");
+});
