57d442b3781cb968fdedb8c3cb1e6b73a72008a2
BenediktDornauer <benedikt.dornauer@outlook.at>
2023-08-01 23:31:22 +0200

    Working commit.

---
+++ server/perfume-js-metrics-explination.md
@@ -0,0 +1,9 @@
+# Metric Definition
+- `Total_time`:  timestamp when the document and all sub-resources have finished loading
+- `FP`: First Paint (FP)
+- `LCP`: Largest Contentful Paint (LCP) 
+- `TBT`: Total Blocking Time (TBT) 
+- `PLT`: Page Load Time (PLT)
+- `Download_time`: Time to download the document
+- `Browser`: Browser name
+
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser'); // Add this line
+const fs = require('fs');
 
 
 const app = express();
@@ -10,14 +11,73 @@ const port = process.env.PORT || 8080;
 app.use(express.static(__dirname));
 app.use(express.static('assets'))
 app.use(express.static('/node_modules/perfume.js/dist/'))
-app.use(bodyParser.urlencoded({ extended: true })); // Add this line
+app.use(bodyParser.urlencoded({extended: true})); // Add this line
 
+function appendDataToFile(filePath, data) {
+    fs.appendFile(filePath, data + '\n', function (err) {
+        if (err) throw err;
+        console.log('Data appended to file');
+    });
+}
 
 app.post('/performance_results', bodyParser.json(), (req, res) => {
-    // print JSON response to output console
-    console.log("result: ", req.body);
+    let result = req.body;
+
+    console.log(result);
+    // Browser,FCP,TTFB,LCP,FID,PLT,fetch_time
+    new_line =
+        result.browser + ',' +
+        result.FCP + ',' +
+        result.TTFB + ',' +
+        result.LCP + ',' +
+        result.FID + ',' +
+        result.PLT + ',' +
+        result.navigationTiming.fetchTime;
+
+    new_line_file = ""
+
+    result.resourceTiming.forEach(element => {
+        if (element.initiatorType === "img") {
+            new_line_file += element.name + ',' + element.duration + '\n'
+        }
+    })
+
+    let browser = result.browser;
+
+    switch (browser) {
+        case 'chrome':
+            appendDataToFile('./results/chrome.csv', new_line);
+            appendDataToFile('./results/chrome_resources.csv', new_line_file);
+            break;
+        case 'firefox':
+            appendDataToFile('./results/firefox.csv', new_line);
+            appendDataToFile('./results/firefox_resources.csv', new_line_file);
+            break;
+        case 'safari':
+            appendDataToFile('./results/safari.csv', new_line);
+            appendDataToFile('./results/safari_resources.csv', new_line_file);
+            break;
+        case 'edge_old':
+            appendDataToFile('./results/edge_old.csv', new_line);
+            appendDataToFile('./results/edge_old_resources.csv', new_line_file);
+            break;
+        case 'opera':
+            appendDataToFile('./results/opera.csv', new_line);
+            appendDataToFile('./results/opera_resources.csv', new_line_file);
+            break;
+        case 'edge_chromium':
+            appendDataToFile('./results/edge_chrome.csv', new_line);
+            appendDataToFile('./results/edge_chrome_resources.csv', new_line_file);
+            break;
+        default:
+            appendDataToFile('./results/other.csv', new_line);
+            appendDataToFile('./results/other_resources.csv', new_line_file);
+            break;
+    }
+
     res.send('POST request to the homepage');
 });
+
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '/index.html'));
